14e3132a8e99995b0cd9770e3a18db9786706826
manuela <maanuhernandez@gmail.com>
2022-09-12 15:41:10 -0300

    entrega final

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 const nombreUsuario = document.getElementById('nombre');
-      selectDia = document.querySelector('#dia');
-      selectMes = document.querySelector('#mes');
-      btnCalcular = document.getElementById('btnCalcular');
-      btnBorrar = document.getElementById('btnBorrar');
-      input = document.querySelector('#inputBox');
-      dias = ['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31'];
-      meses = ['01','02','03','04','05','06','07','08','09','10','11','12'];
+selectDia = document.querySelector('#dia');
+selectMes = document.querySelector('#mes');
+btnCalcular = document.getElementById('btnCalcular');
+btnBorrar = document.getElementById('btnBorrar');
+dias = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31'];
+meses = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+contenedor = document.querySelector('#contenedorTarjetas');
+container = document.querySelector('#cardContainer');
 
 function cargarSelect(array, select) {
     array.forEach(element => {
@@ -13,7 +14,7 @@ function cargarSelect(array, select) {
         select.innerHTML += option;
     })
 };
-    
+
 cargarSelect(dias, selectDia);
 cargarSelect(meses, selectMes);
 
@@ -24,46 +25,124 @@ btnCalcular.addEventListener('click', (event) => {
         nombre: nombreUsuario.value,
         dia: parseInt(selectDia.value),
         mes: parseInt(selectMes.value),
-    }; 
-  
-    const signos = [
-        {signo: 'Aries', fecha: ((datos.dia >= 21 && datos.mes == 3) || (datos.dia <= 19 && datos.mes == 4)), elemento: 'Fuego'}, 
-        {signo: 'Tauro', fecha: ((datos.dia >= 20 && datos.mes == 4) || (datos.dia <= 20 && datos.mes == 5)), elemento: 'Tierra'},
-        {signo: 'Géminis', fecha: ((datos.dia >= 21 && datos.mes == 5) || (datos.dia <= 20 && datos.mes == 6)), elemento: 'Aire'},
-        {signo: 'Cáncer', fecha: ((datos.dia >= 21 && datos.mes == 6) || (datos.dia <= 22 && datos.mes == 7)), elemento: 'Agua'},
-        {signo: 'Leo', fecha: ((datos.dia >= 23 && datos.mes == 7) || (datos.dia <= 22 && datos.mes == 8)), elemento: 'Fuego'},
-        {signo: 'Virgo', fecha: ((datos.dia >= 23 && datos.mes == 8) || (datos.dia <= 22 && datos.mes == 9)), elemento: 'Tierra'},
-        {signo: 'Libra', fecha: ((datos.dia >= 23 && datos.mes == 9) || (datos.dia <= 22 && datos.mes == 10)), elemento: 'Aire'},
-        {signo: 'Escorpio', fecha: ((datos.dia >= 23 && datos.mes == 10) || (datos.dia <= 21 && datos.mes == 11)), elemento: 'Agua'},
-        {signo: 'Sagitario', fecha: ((datos.dia >= 22 && datos.mes == 11) || (datos.dia <= 21 && datos.mes == 12)), elemento: 'Fuego'},
-        {signo: 'Capricornio', fecha: ((datos.dia >= 22 && datos.mes == 12) || (datos.dia <= 19 && datos.mes == 1)), elemento: 'Tierra'},
-        {signo: 'Acuario', fecha: ((datos.dia >= 20 && datos.mes == 1) || (datos.dia <= 18 && datos.mes == 2)), elemento: 'Aire'},
-        {signo: 'Piscis', fecha: ((datos.dia >= 19 && datos.mes == 2) || (datos.dia <= 20 && datos.mes == 3)), elemento: 'Agua'},
+    };
+
+    localStorage.setItem('Nombre', nombreUsuario.value);
+    localStorage.setItem('Dia de nacimiento', selectDia.value);
+    localStorage.setItem('Mes de nacimiento', selectMes.value);
+    sessionStorage.setItem('Nombre', nombreUsuario.value);
+    sessionStorage.setItem('Dia de nacimiento', selectDia.value);
+    sessionStorage.setItem('Mes de nacimiento', selectMes.value);
+
+    const signos = [{
+            signo: 'Aries',
+            fecha: ((datos.dia >= 21 && datos.mes == 3) || (datos.dia <= 19 && datos.mes == 4)),
+            elemento: 'el Fuego'
+        },
+        {
+            signo: 'Tauro',
+            fecha: ((datos.dia >= 20 && datos.mes == 4) || (datos.dia <= 20 && datos.mes == 5)),
+            elemento: 'la Tierra'
+        },
+        {
+            signo: 'Géminis',
+            fecha: ((datos.dia >= 21 && datos.mes == 5) || (datos.dia <= 20 && datos.mes == 6)),
+            elemento: 'el Aire'
+        },
+        {
+            signo: 'Cáncer',
+            fecha: ((datos.dia >= 21 && datos.mes == 6) || (datos.dia <= 22 && datos.mes == 7)),
+            elemento: 'el Agua'
+        },
+        {
+            signo: 'Leo',
+            fecha: ((datos.dia >= 23 && datos.mes == 7) || (datos.dia <= 22 && datos.mes == 8)),
+            elemento: 'el Fuego'
+        },
+        {
+            signo: 'Virgo',
+            fecha: ((datos.dia >= 23 && datos.mes == 8) || (datos.dia <= 22 && datos.mes == 9)),
+            elemento: 'la Tierra'
+        },
+        {
+            signo: 'Libra',
+            fecha: ((datos.dia >= 23 && datos.mes == 9) || (datos.dia <= 22 && datos.mes == 10)),
+            elemento: 'el Aire'
+        },
+        {
+            signo: 'Escorpio',
+            fecha: ((datos.dia >= 23 && datos.mes == 10) || (datos.dia <= 21 && datos.mes == 11)),
+            elemento: 'el Agua'
+        },
+        {
+            signo: 'Sagitario',
+            fecha: ((datos.dia >= 22 && datos.mes == 11) || (datos.dia <= 21 && datos.mes == 12)),
+            elemento: 'el Fuego'
+        },
+        {
+            signo: 'Capricornio',
+            fecha: ((datos.dia >= 22 && datos.mes == 12) || (datos.dia <= 19 && datos.mes == 1)),
+            elemento: 'la Tierra'
+        },
+        {
+            signo: 'Acuario',
+            fecha: ((datos.dia >= 20 && datos.mes == 1) || (datos.dia <= 18 && datos.mes == 2)),
+            elemento: 'el Aire'
+        },
+        {
+            signo: 'Piscis',
+            fecha: ((datos.dia >= 19 && datos.mes == 2) || (datos.dia <= 20 && datos.mes == 3)),
+            elemento: 'el Agua'
+        },
     ];
-    
-    let encontrar = signos.filter((datos,signos) => signos.fecha === datos.dia && signos.fecha === datos.mes);
-       
-    const encontrarSigno = encontrar.find((signo) => signo.fecha == true);
- 
-    let signo = encontrarSigno.signo;
-    let elemento = encontrarSigno.elemento;
 
-    swal({
-        title: "Bienvenido/a " + nombreUsuario.value +"!",
-        text: "Tu signo es " + encontrarSigno.signo + '. Y su elemento es ' + encontrarSigno.elemento,
-        icon: "success",
-        buttons: ["OK", "Leer mas"],
-      });
+    const filtrarSignos = signos.filter((datos, signos) => signos.fecha === datos.dia && signos.fecha === datos.mes);
+    const encontrarSigno = filtrarSignos.find((signo) => signo.fecha == true);
 
-      localStorage.setItem('Nombre',nombreUsuario.value);
-      localStorage.setItem('Dia de nacimiento',selectDia.value);
-      localStorage.setItem('Mes de nacimiento',selectMes.value);
-      sessionStorage.setItem('Nombre',nombreUsuario.value);
-      sessionStorage.setItem('Dia de nacimiento',selectDia.value);
-      sessionStorage.setItem('Mes de nacimiento',selectMes.value);
+    function signoFilter(array) {
+        let signo = encontrarSigno.signo;
+        if (!signo) {
+            return array;
+        } else {
+            return array.filter((e) => signo === e.signo);
+        }
+    };
+
+    function createHTML(array) {
+        contenedor.innerHTML = ''
+        container.innerHTML = ''
+        array.forEach((e) => {
+            const card = `
+                <div class="alert alert-light alert-dismissible fade show position-absolute top-50 start-50 translate-middle" role="alert">
+                    <div class="card mb-3 border-0 lh-lg position-absolute top-50 start-50 translate-middle">
+                    <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="${e.image}" class="img-fluid" alt="${e.signo}">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body text-secondary">
+                        <h1 class="card-title">Bienvendia/o ${nombreUsuario.value}</h1>
+                        <h6 class="card-title">Tu signo es ${e.signo} y su elemento es ${e.elemento}</h6>
+                        <h6 class="card-text"></h6>
+                        <p class="card-text"> ${e.caracteristicas}</p>
+                        </div>
+                    </div>
+                    </div>
+                    </div>
+                <button type="button" class="btn-close btn btn-outline-warning bg-warning p-2 m-3" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>`
+            container.innerHTML += card
+        })
+    };
+
+    fetch('js/data.json')
+        .then((response) => response.json())
+        .then((data) => {
+            createHTML(signoFilter(data))
+        });
 });
 
-btnBorrar.addEventListener('click',()=>{
+btnBorrar.addEventListener('click', () => {
     sessionStorage.clear();
     localStorage.clear();
     document.getElementById('form').reset();
@@ -71,11 +150,5 @@ btnBorrar.addEventListener('click',()=>{
         buttons: false,
         timer: 2000,
         icon: "success",
-      });
+    });
 });
-
-fetch('https://jsonplaceholder.typicode.com/posts')
-.then((Response)=>Response.json())
-.then((data)=>{
-    console.log(data);
-})
